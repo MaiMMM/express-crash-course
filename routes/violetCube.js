06cee34c4b47efaf060130678a25ea4bb71f4163
Chris He <h1119623520@gmail.com>
2023-02-06 15:03:43 -0500

    Update violetCube.js

--- routes/violetCube.js
+++ routes/violetCube.js
@@ -51,7 +51,7 @@ const STR9 = "STR: +9%"
 const HP9 = "MAX HP: +9%"
 const MP9 = "MAX MP: +9%"
 const DEF9 = "DEF: +9%"
-const ALL6 = "All Stats +6%"
+const ALL6 = "All Stats: +6%"
 const IG20U = "5% chance to ignore 20% of monster damage dealt"
 const IG40U = "5% chance to ignore 40% of monster damage dealt"
 const RECOV = "HP Recovery Items and Skills: +30%"
@@ -81,10 +81,193 @@ router
   .get((req, res) => {
     // console.log(req.user)
     var type = req.query.type; var stat = req.query.stat; var num = req.query.num; var numOfTry = req.query.numOfTry;
-    main(type,stat,num,numOfTry,res); //main function
-
+    // main(type,stat,num,numOfTry,res); //main function
     res.send(`Get User With ID ${req.params}`)
   })
+
+
+//-----------------------------------Functions for rolling stat----------------------------------------------------
+// Helper function for prime_random_select() and unprime_random_select()
+function pickfromarr(lib, pos){
+    if(lib.length !== pos.length){console.log("lib pos length different");console.log(lib.length);console.log(pos.length)}
+
+    var sum = 0.0;
+    for(let i = 0; i < pos.length; i++){ sum = sum + pos[i];}
+    // console.log(sum)
+    var random_var = Math.random() * sum; 
+    // console.log(random_var);
+
+    var new_sum = 0.0;
+    for(let i = 0; i < pos.length; i++){ 
+        new_sum += pos[i];
+        if(new_sum > random_var){ 
+            // console.log("new_sum = " + new_sum + "  random var = " +random_var)
+            return lib[i]; 
+
+        }
+
+    }
+    return lib[lib.length-1];
+}
+
+// input: typeOfEquip
+// output random stat line
+function prime_random_select(typeOfEquip){
+    var hat_lib = [DEX,    INT,    STR,    LUK,    HP,     MP,     DEF,    ALL,    IG20,   IG40,   CD1S,   CD2S,   "Enable the Decent Advanced Bless skill"]
+    var hat_pos = [8.88888,8.88888,8.88888,8.88888,8.88888,8.88888,8.88888,6.66666,6.66666,6.66666,6.66666,4.44444,6.66666]
+
+    var top_lib = [DEX,   INT,   STR,   LUK,   HP,    MP,    DEF,   ALL,   IG20,  IG40,  INVI3S,INVI4P]
+    var top_pos = [9.3023,9.3023,9.3023,9.3203,9.3202,9.3202,9.3203,6.9767,6.9767,6.9767,6.9767,6.9767]                                      
+
+    var bottom_lib = [DEX,    INT,    STR,    LUK,    HP,     MP,     DEF,     ALL,   IG20,  IG40]
+    var bottom_pos = [10.8108,10.8108,10.8108,10.8108,10.8108,10.8108,10.8108,8.1081, 8.1081,8.1081]
+
+    var glove_lib = [DEX,   INT,   STR,   LUK,   HP,    MP,    DEF,   CD,    ALL,   IG20,  IG40,   "Enable the Decent Speed Infusion skill"  ]
+    var glove_pos = [9.0909,9.0909,9.0909,9.0909,9.0909,9.0909,9.0909,9.0909,6.8181,6.8181,6.8181, 6.8181]
+
+    var shoe_lib = [DEX,INT,STR,LUK,HP,MP,DEF,ALL,IG20,IG40,"Enable the Decent Combat Orders skill"]
+    var shoe_pos = [10, 10, 10, 10, 10,10,10,7.5, 7.5, 7.5, 7.5]
+
+    var cape_belt_shoulder_lib = [DEX,    INT,    STR,    LUK,    HP,     MP,     DEF,    ALL,   IG20,  IG40]
+    var cape_belt_shoulder_pos = [10.8108,10.8108,10.8108,10.8108,10.8108,10.8108,10.8108,8.1081,8.1081,8.1081]
+
+    //Face Accessory, Eye Accessory, Ring, Earring, Pendant
+    var Accessory_lib = [DEX,   INT,   STR,   LUK,   HP,    MP,    DEF,   ALL,   MP15,  MP30,  MESO,  DROP]
+    var Accessory_pos = [9.3023,9.3023,9.3023,9.3023,9.3023,9.3023,9.3023,6.9767,6.9767,6.9767,6.9767,6.9767  ]
+
+    var weapon_lib = [DEX,  INT,  STR,  LUK,  ALL,  ATT,  MATT, CRIT, DMG,  ATTLV, MATTLV,IED35,IED40,BOSS30,BOSS35,BOSS40]
+    var weapon_pos = [9.756,9.756,9.756,9.756,7.317,4.878,4.878,4.878,4.878,4.878, 4.878, 4.878,4.878,4.878, 4.878, 4.878]
+    
+    var secondary_lib = [DEX,   INT,   STR,   LUK,   ALL,   IG20,  IG40, ATT,  MATT, CRIT, DMG,  ATTLV, MATTLV,IED35,IED40,BOSS30,BOSS35,BOSS40]
+    var secondary_pos = [8.5106,8.5106,8.5106,8.5106,6.3829,6.3829,6.3829,4.2553,4.2553,4.2553,4.2553,4.2553,4.2553,4.2553,4.2553,4.2553,4.2553,4.2553 ]
+
+    var badge_heart_lib = [DEX,    INT,    STR,    LUK,    HP,     MP,     DEF,    ALL]
+    var badge_heart_pos = [12.9032,12.9032,12.9032,12.9032,12.9032,12.9032,12.9032,9.6774]
+    
+    var emblem_lib = [DEX,    INT,    STR,    LUK,    ALL,   ATT,   MATT,  CRIT,  DMG,   ATTLV, MATTLV,IED35,IED40]
+    var emblem_pos = [11.4285,11.4285,11.4285,11.4285,8.5714,5.7142,5.7142,5.7142,5.7142,5.7142,5.7142,5.7142,5.7142]
+  
+    var str = typeOfEquip;
+
+    if (str === "hat"){return(pickfromarr(hat_lib,hat_pos))}
+    if (str === "top"){return(pickfromarr(top_lib,top_pos))}
+    if (str === "buttom"){return(pickfromarr(bottom_lib,bottom_pos))}
+    if (str === "glove"){return(pickfromarr(glove_lib,glove_pos))}
+    if (str === "shoe"){return(pickfromarr(shoe_lib,shoe_pos))}
+    if (str === "belt" || str === "cape" || str === "shoulder"){return(pickfromarr(cape_belt_shoulder_lib,cape_belt_shoulder_pos))}
+    if (str === "face" || str === "pendant" || str === "eye" || str === "ring" || str === "earring"){return(pickfromarr(Accessory_lib,Accessory_pos))}
+    if (str === "weapon"){return(pickfromarr(weapon_lib,weapon_pos))}
+    if (str === "secondary"){return(pickfromarr(secondary_lib,secondary_pos))}
+    if (str === "badge" || str === "heart" ){return(pickfromarr(badge_heart_lib,badge_heart_pos))}
+    if (str === "emblem"){return(pickfromarr(emblem_lib,emblem_pos))}
+}
+
+// input: typeOfEquip
+// output random stat line
+function unprime_random_select(typeOfEquip){
+    var hat_lib = [DEX9,   INT9,   STR9,   LUK9,   HP9,     MP9,     DEF9,   ALL6,   IG20U,  IG40U,RECOV,   "Enable the Decent Mystic Door skill"]
+    var hat_pos = [8.9285, 8.9285, 8.9285, 8.9285, 10.7142, 10.7142, 7.1428, 7.1428, 7.1428, 7.1428,7.1428,  7.1428]
+
+    var top_lib = [DEX9,   INT9,   STR9,   LUK9,   HP9,     MP9,     DEF9,   ALL6,   IG20U,  IG40U, INVI2S,INVI2P,REFLECT50,REFLECT70,RECOV]
+    var top_pos = [7.5757, 7.5757, 7.5757, 7.5757, 9.0909,	9.0909,  6.0606, 6.0606, 6.0606, 6.0606,6.0606,6.0606,6.0606,   3.0303,   6.0606]                                      
+
+    var bottom_lib = [DEX9,   INT9,   STR9,   LUK9,  HP9,    MP9,    DEF9,   ALL6,   IG20U,  IG40U, RECOV, "Enable the Decent Decent Hyper Body skill"]
+    var bottom_pos = [8.9285, 8.9285, 8.9285, 8.9285,10.8108,10.8108,7.1428, 7.1428, 7.1428, 7.1428,7.1428, 7.1428]
+
+    var glove_lib = [DEX9,   INT9,   STR9,   LUK9,  HP9,    MP9,    DEF9,   ALL6,  STR1,  DEX1,  INT1,  LUK1,  IG20U,  IG40U, RECOV, "Enable the Decent Sharp Eyes skill"]
+    var glove_pos = [8.3333, 8.3333, 8.3333, 8.3333,10,     10,     6.6666, 6.6666,1.6666,1.6666,1.6666,1.6666,6.6666, 6.6666,6.6666, 6.6666]
+
+    var shoe_lib = [DEX9,   INT9,   STR9,   LUK9,  HP9,    MP9,    DEF9,   ALL6,   IG20U,  IG40U, RECOV, "Enable the Decent Decent Hyper Body skill"]
+    var shoe_pos = [8.9285, 8.9285, 8.9285, 8.9285,10.7142 ,10.7142,7.1428,7.1428, 7.1428, 7.1428, 7.1428,7.1428]
+
+    var cape_belt_shoulder_lib = [DEX9,  INT9,  STR9,  LUK9,  HP9,    MP9,    DEF9,  ALL6,  IG20U, IG40U, RECOV]
+    var cape_belt_shoulder_pos = [9.6153,9.6153,9.6153,9.6153,11.5384,11.5384,7.6923,7.6923,7.6923,7.6923,7.6923]
+
+    //Face Accessory, Eye Accessory, Ring, Earring, Pendant
+    var Accessory_lib = [DEX9,   INT9,   STR9,   LUK9,   HP9,    MP9,    DEF9,  ALL6,  RECOV]
+    var Accessory_pos = [11.3636,11.3636,11.3636,11.3636,13.6363,13.6363,9.0909,9.0909,9.0909]
+
+    var weapon_lib = [DEX9,    INT9,    STR9,    LUK9,   ALL6,    ATT9,  MATT9, CRIT9,  DMG9, IED30,  BOSS30]
+    var weapon_pos = [11.6279, 11.6279, 11.6279, 11.6279,9.3023,  6.9767,6.9767,9.3023, 6.9767,6.9767,6.9767]
+
+    var secondary_lib = [DEX9,  INT9,  STR9,  LUK9, ALL6,  IG20U, IG40U,  ATT9,  MATT9, CRIT9,  DMG9, IED30,  BOSS30]
+    var secondary_pos = [9.8039,9.8039,9.8039,9.8039,7.8431,7.8431,7.8431,5.8823,5.8823,7.8431,5.8823,5.8823, 5.8823]
+
+    var badge_heart_lib = [DEX9,   INT9,   STR9,   LUK9,   HP9,    MP9,    DEF9,  ALL6,  RECOV]
+    var badge_heart_pos = [11.3636,11.3636,11.3636,11.3636,13.6363,13.6363,9.0909,9.0909,9.0909]
+
+    var emblem_lib = [DEX9,   INT9,   STR9,   LUK9,   ALL6,  ATT9,  MATT9, CRIT9,  DMG9, IED30  ]
+    var emblem_pos = [12.5,   12.5,   12.5,   12.5,   10,    7.5,   7.5,  10,      7.5,  7.5]
+
+    var str = typeOfEquip;
+
+    if (str === "hat"){return(pickfromarr(hat_lib,hat_pos))}
+    if (str === "top"){return(pickfromarr(top_lib,top_pos))}
+    if (str === "buttom"){return(pickfromarr(bottom_lib,bottom_pos))}
+    if (str === "glove"){return(pickfromarr(glove_lib,glove_pos))}
+    if (str === "shoe"){return(pickfromarr(shoe_lib,shoe_pos))}
+    if (str === "belt" || str === "cape" || str === "shoulder"){return(pickfromarr(cape_belt_shoulder_lib,cape_belt_shoulder_pos))}
+    if (str === "face" || str === "pendant" || str === "eye" || str === "ring" || str === "earring"){return(pickfromarr(Accessory_lib,Accessory_pos))}
+    if (str === "weapon"){return(pickfromarr(weapon_lib,weapon_pos))}
+    if (str === "secondary"){return(pickfromarr(secondary_lib,secondary_pos))}
+    if (str === "badge" || str === "heart" ){return(pickfromarr(badge_heart_lib,badge_heart_pos))}
+    if (str === "emblem"){return(pickfromarr(emblem_lib,emblem_pos))}
+}
+
+// input: possibility (0-100)
+// output: true or false (true -> isPrime; false -> isNotPrime)
+function check_prime(num){
+    var limit = Math.random()*100;
+    if(limit < num){return true;}
+    return false;
+}
+
+//input: typeOfEquip, primeRate(%chance of the line being a prime line)
+//output: single stat line
+function runOnce(typeOfEquip, primeRate){
+    let isPrime = check_prime(primeRate);
+    let ret = "";
+    // console.log("isPrime:" + isPrime);
+
+    if(isPrime){
+        ret = prime_random_select(typeOfEquip);
+    }else{
+        ret = unprime_random_select(typeOfEquip)
+    }
+
+    //error checking
+    if(ret === "" || ret == null){
+        console.log("typeOfEquip is wrong or I fucked up");return -1; 
+    }else{
+        return ret;
+    }
+
+}
+
+
+//-------------------------------------functions for calculating if a roll passes the desired stat--------------------------------------------------
+// example: calculatedMatchingStat("STR: + 12%", "STR") = 12
+// example: calculatedMatchingStat("STR: + 12%", "DEX") = 0
+function calculateMatchingStat(statLine, desiredStat){
+    ;
+}
+
+// input: desiredStat,desiredTotalStat, a list of stats given by a single roll 
+// (e.g.)
+// 'STR: +12%',
+// 'MAX MP: +9%',
+// 'STR: +9%',
+// 'DEX: +9%',
+// 'DEX per 10 Character Level: +1',
+// 'MAX HP: +9%'
+//
+// output: true / false
+function ifRollPassDesiredNum(desiredStat,desiredNum, listOfRolledStat){
+    console.log(listOfRolledStat);
+}
+
+//---------------------------------------------------------------------------------------
+
 
 
 function main(typeOfEquip,stat,desiredNum,numOfTry,res){
@@ -94,12 +277,18 @@ function main(typeOfEquip,stat,desiredNum,numOfTry,res){
     else if(numOfTry < 100 || numOfTry > 100000){res.statusCode = 401; res.send('Wrong numOfTry input'); isValid = false}
 
     if(isValid){
-        
+        for(let i = 0; i < numOfTry; i++){
+            let listOfRolledStat = [runOnce(typeOfEquip,100),runOnce(typeOfEquip,1),runOnce(typeOfEquip,1),
+                                    runOnce(typeOfEquip,10),runOnce(typeOfEquip,1),runOnce(typeOfEquip,1)];
+            ifRollPassDesiredNum(stat,30,listOfRolledStat);
+        }
     }
-
 }
 
+// console.log(runOnce("glove", 100));
 
+
+main("glove","dex",30,100, "any");
 
 
 
